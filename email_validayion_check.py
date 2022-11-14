0b509d7bd1413a370f79194ec8509cb6f103f0eb
Rakibul Hasan <99094220+0rakib0@users.noreply.github.com>
2022-11-14 23:04:39 +0600

    email verification system

---
+++ email_validayion_check.py
@@ -0,0 +1,33 @@
+
+email = input(" Enter your email: " )
+
+k,j,d=0,0,0
+
+if len(email)>=6:
+    if email[0].isalpha():
+        if ("@" in email) and (email.count("@")==1):
+            if (email[-4]==".") ^ (email[-3]=="."):
+                for i in email:
+                    if i == i.isspace():
+                        k = 1
+                    elif i.isalpha():
+                        i == i.upper()
+                        j = 1
+                    elif i.isdigit():
+                        continue
+                    elif i == "_" or i == "." or i == "@":
+                        continue
+                    else:
+                        d = 1
+                if k == 1 or j == 1 or d == 1:
+                    print(" Wrong Email in 5 condition")
+                else:
+                    print(" This email is Valid")
+            else:
+                print(" Wrong email in 4 condition")
+        else:
+            print(" Wrong Input in 3 condition")
+    else:
+        print(" Wrong Email in 2 condition")
+else:
+    print(" Wrong Email in 1 condition")
